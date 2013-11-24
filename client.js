c55dab77320863f9ea386b83f689482a2e1a61d2
Sophia Chung <chungs1@berkeley.edu>
2013-11-24 07:49:02 -0800

    made it pretty

--- client.js
+++ client.js
@@ -11,9 +11,13 @@ socket.on("chat_callback", function(data) {
 
 $("#join").click(function() {
 	$("#join").attr('disabled', 'disabled');
-	$(".set_username").fadeOut();
+	$("#set_username").fadeOut();
+	$("#join").fadeOut();
 	$('.chat').fadeIn(2700);
+	$("#user").fadeIn();
+	$("#user").innerHTML = "PSYCH IT'S TOTALLY ANONYMOUS"
 });
+
 
 $("#chatbox").keypress(function(e) {
 	return false;
@@ -23,13 +27,37 @@ $("#chatbox_content").keypress(function(e) {
 	if(e.which == 13) {
 		var content = $("#chatbox_content").val();
 		if(content != '') {
-			var message = "<li>" + content + "<br><hr></li>";
+			var message = "<p>" + content + "</p>";
 			$('#chatbox').append(message);
 			$("#chatter").scrollTop($("#chatbox")[0].scrollHeight);
 			socket.emit('chat_message', message);
 			$("#chatbox_content").val("");
 		}
 	}
+});
+
+$("#blue").on("click", function(e) {
+	$("input:focus").css({'backgroundColor':'#59B4FF'});
+	$("input").css({'backgroundColor': '#59B4FF'});
+	$("body").css({'backgroundColor': "#59B4FF"});
+});
+
+$("#violet").on("click", function(e) {
+	$("input:focus").css({'backgroundColor':'#F759FF'});
+	$("input").css({'backgroundColor': '#F759FF'});
+	$("body").css({'backgroundColor': "#F759FF"});
+});
+
+$("#orange").on("click", function(e) {
+	$("input:focus").css({'backgroundColor':'#FFB029'});
+	$("input").css({'backgroundColor': '#FFB029'});
+	$("body").css({'backgroundColor': "#FFB029"});
+});
+
+$("#green").on("click", function(e) {
+	$("input:focus").css({'backgroundColor':'#49C445'});
+	$("input").css({'backgroundColor': '#49C445'});
+	$("body").css({'backgroundColor': "#49C445"});
 });
 /*var markers = {};
 var connect = new WebSocket("ws://10.142.36.1:8000");
